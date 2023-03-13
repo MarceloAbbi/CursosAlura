2721d1e613b48c28a604c9e2b6b1e97896286297
Marcelo Abbi <marcelo.abbi@outlook.com>
2023-03-12 23:48:16 -0300

    componentes

--- Reactjs/alura-books/src/App.js
+++ Reactjs/alura-books/src/App.js
@@ -4,11 +4,7 @@ function App() {
   return (
     <div className='App'>
       <header className='App-header'>
-        <div className='logo'>
-          <img src={logo} alt='Logo Alura Books'></img>
-          <p><strong>Alura</strong>Books</p>
-
-        </div>
+        
       </header>
     </div>
   );
