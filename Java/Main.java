247e30a3e457093c86c30726480385410d448df8
Marcelo Abbi <marcelo.abbi@outlook.com>
2023-10-16 14:45:12 -0300

    mudanças

---
+++ Java/Main.java
@@ -0,0 +1,22 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("This is ScreenMatch");
+        System.out.println("Movie: Top Gun Maverick");
+
+        int releaseDate = 2022;
+        System.out.println("Release date: " + releaseDate);
+        boolean planIncluded = true;
+        double movieRating = 8.1;
+        System.out.println("Movie Rating IMDB: " + movieRating);
+
+        double average = (9.8 + 6.3 + 8.0) /3;
+        System.out.println("Movie Rating Alura: " + average);
+
+        String summary;
+        summary = "Adventure movie with Tom Cruise and  flying Jets.";
+        System.out.println(summary);
+
+    }
+}
